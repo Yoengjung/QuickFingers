f06c85b5d2882b8a3d5e8850d6b23cf672a60f2f
Yoengjung <18681002@konyang.ac.kr>
2023-08-16 10:01:33 +0900

    fix: 불필요한 코드 제거

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,14 +2,3 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
 });
-
-module.exports = {
-  devServer: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-      },
-    },
-  },
-};
